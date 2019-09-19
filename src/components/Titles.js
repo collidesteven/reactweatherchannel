7f9dc01dd49d1bd75593faccb0023ccb0ad4be2a
Steven Bui <buisteven.collide@gmail.com>
2019-09-19 16:41:11 -0700

    Weather App Complete

---
+++ src/components/Titles.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Titles extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1 className='title-container__title'>Weather Finder</h1>
+        <h3 className='title-container__subtitle'>Find out temperature, conditions <br/> and more...</h3>
+      </div>
+    );
+  }
+}
+
+export default Titles;
